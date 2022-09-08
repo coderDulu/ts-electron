610debe9a0c23375f70e3630755a4f2f8dfd8e28
jf <jiang.feng@huaxinda.tech>
2022-09-08 15:56:03 +0800

    docs: delete some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import MainToRender from './components/MainToRender';
-import RenderToMain from './components/RenderToMain';
-import RenderToWayMain from './components/RenderToWayMain';
 
 function App() {
   return (
     <div className="App">
-      {/* React-electron-template1 */}
-      <RenderToMain/>
-      <RenderToWayMain/>
-      <MainToRender/>
+      React-Ts-electron-template
     </div>
   );
 }
