12a6af51faadef32a5695beeee48099da7e71bb3
jf <jiang.feng@huaxinda.tech>
2022-09-29 17:00:52 +0800

    use webpack and remove create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      React-Ts-electron-template
+      React-Ts-electron-template 222211111113333
     </div>
   );
 }
