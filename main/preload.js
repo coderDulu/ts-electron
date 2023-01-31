9483939b43beb1936ae95452ac37f0b495fa88e6
coderDulu <112756487+coderDulu@users.noreply.github.com>
2023-01-31 17:42:36 +0800

    Update preload.js

--- main/preload.js
+++ main/preload.js
@@ -4,4 +4,19 @@ contextBridge.exposeInMainWorld('electronAPI', {
   setTitle: (title) => ipcRenderer.send('set-title', title),
   openFile: () => ipcRenderer.invoke('dialog:openFile'),
   handleCount: (callback) => ipcRenderer.on('update-counter', callback),
+  
+  /** 取消监听 */
+  removeListener: (names) => {
+
+    // 数组为空移除所有监听
+    if (!names.length) {
+      ipcRenderer.removeAllListeners();
+    } else {
+      names.forEach(name => {
+        ipcRenderer.removeAllListeners(name)
+      })
+    }
+
+    return null
+  }
 })
