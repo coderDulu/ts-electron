d24b4a3ce012d8f38a9ce5b89a8dbb24c592b600
dudulu <1791105501@qq.com>
2023-04-01 14:00:22 +0800

    update .gitignore

--- out/preload/preload.js
+++
@@ -1,1 +0,0 @@
-"use strict";const e=require("electron");e.contextBridge.exposeInMainWorld("electronAPI",{openFile:()=>e.ipcRenderer.invoke("dialog:openFile"),openDevTool:()=>e.ipcRenderer.send("open-dev"),removeListener:r=>(r.length&&r.forEach(n=>{e.ipcRenderer.removeAllListeners(n)}),null)});
